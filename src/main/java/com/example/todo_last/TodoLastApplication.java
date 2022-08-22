05dbae3668e490fe1c59267678a5dedd6da822ca
Hong ! <alswjd3813@naver.com>
2022-08-22 14:19:02 +0900

    Add files via upload

---
+++ src/main/java/com/example/todo_last/TodoLastApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo_last;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoLastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoLastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/todo_last/TodoLastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todo_last;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoLastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
